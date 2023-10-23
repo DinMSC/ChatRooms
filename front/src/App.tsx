08d4ac6e6357bf2274978f58ad4e4a33b029146c
Din Mušović <93013218+DinMSC@users.noreply.github.com>
2023-10-23 13:26:30 +0200

    finished routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
-    return <div className='App'>Hello</div>;
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={<Chat />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='*' element={<Navigate to='/' />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
