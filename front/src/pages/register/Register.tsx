cfc10a2aaafcf45df7a0acc127debd93d7b5456f
DinMSC <dinmusovic2021@gmail.com>
2023-10-28 15:39:20 +0200

    delete

--- front/src/pages/register/Register.tsx
+++ front/src/pages/register/Register.tsx
@@ -13,8 +13,14 @@ const Register = () => {
         },
         validationSchema: SignupSchema,
         onSubmit: (values) => {
-            console.log(values);
-            //add axios post on register route!
+            axios
+                .post('http://:localhost:8000/api/register', values)
+                .then((response) => {
+                    console.log(response.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         },
     });
 
